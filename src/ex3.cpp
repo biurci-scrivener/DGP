27c64931d6e9202ffab1dd0e03905b61c761129f
Daniel Scrivener <dscriv@bu.edu>
2024-09-16 16:29:45 -0400

    ex3 changes

--- src/ex3.cpp
+++ src/ex3.cpp
@@ -5,6 +5,7 @@ void Exercise3::generate_curve_simple() {
     std::cout << "Generating simple curve" << std::endl;
     std::default_random_engine generator;
     std::uniform_real_distribution<double> distribution(0., 5 * 3e-2);
+    generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
 
     Point center = {0., 0., 0.};
     double radius = 1;
@@ -66,6 +67,7 @@ void Exercise3::generate_curve_3d() {
     // TODO: ADD YOUR CODE HERE
 
     Curve = polyscope::registerCurveNetwork("curve", points, edges);
+    // generate_world_axes(); // uncomment to visualize world axes
 }
 
 void Exercise3::laplacian_smoothing() {
@@ -89,4 +91,42 @@ glm::vec3 Exercise3::CircumscribedCircle(const Point& a, const Point& b, const P
     // TODO: ADD YOUR CODE HERE
 
     return center;
+}
+
+void Exercise3::generate_world_axes() {
+    PCPointCloud * ax_x = new PCPointCloud(1);
+    PointPositionGeometry * geo_x = new PointPositionGeometry(*ax_x);
+    geo_x->positions[0] = {0.,0.,-1.};
+    PointData<Point> ax_x_q(*ax_x);
+    ax_x_q[0] = {0.25,0.,0.};
+    auto pc_x = polyscope::registerPointCloud("World x-axis", geo_x->positions);
+    pc_x->setPointColor({0.,0.,0.});
+    auto vis_x = pc_x->addVectorQuantity("Axes", ax_x_q, VectorType::AMBIENT);
+    vis_x->setVectorColor({1.,0.,0.});
+    vis_x->setVectorRadius(0.01);
+    vis_x->setEnabled(true);
+
+    PCPointCloud * ax_y = new PCPointCloud(1);
+    PointPositionGeometry * geo_y = new PointPositionGeometry(*ax_y);
+    geo_y->positions[0] = {0.,0.,-1.};
+    PointData<Point> ax_y_q(*ax_y);
+    ax_y_q[0] = {0.,0.25,0.};
+    auto pc_y = polyscope::registerPointCloud("World y-axis", geo_y->positions);
+    pc_y->setPointColor({0.,0.,0.});
+    auto vis_y = pc_y->addVectorQuantity("Axes", ax_y_q, VectorType::AMBIENT);
+    vis_y->setVectorColor({0.,1.,0.});
+    vis_y->setVectorRadius(0.01);
+    vis_y->setEnabled(true);
+
+    PCPointCloud * ax_z = new PCPointCloud(1);
+    PointPositionGeometry * geo_z = new PointPositionGeometry(*ax_z);
+    geo_z->positions[0] = {0.,0.,-1.};
+    PointData<Point> ax_z_q(*ax_z);
+    ax_z_q[0] = {0.,0.,0.25};
+    auto pc_z = polyscope::registerPointCloud("World z-axis", geo_z->positions);
+    pc_z->setPointColor({0.,0.,0.});
+    auto vis_z = pc_z->addVectorQuantity("Axes", ax_z_q, VectorType::AMBIENT);
+    vis_z->setVectorColor({0.,0.,1.});
+    vis_z->setVectorRadius(0.01);
+    vis_z->setEnabled(true);
 }
--- src/include/ex3.h
+++ src/include/ex3.h
@@ -2,18 +2,24 @@
 #define CURVESMOOTHING_HH
 
 #include "polyscope/curve_network.h"
+#include "polyscope/point_cloud.h"
+#include "geometrycentral/pointcloud/point_cloud.h"
+#include "geometrycentral/pointcloud/point_position_geometry.h"
 
 using namespace polyscope;
+using geometrycentral::pointcloud::PointPositionGeometry;
+using geometrycentral::pointcloud::PointData;
 
 class Exercise3 {
   public:
     using Point = glm::vec3;
+    using PCPointCloud = geometrycentral::pointcloud::PointCloud;
 
     Exercise3() : Curve(nullptr) {}
     ~Exercise3() {}
     size_t num_vertices = 30;
     float epsilon = 0.01;
-    int smooth_type = 1;
+    int smooth_type = 0;
     int num_iter = 10;
 
     void apply_smooth() {
@@ -28,6 +34,7 @@ class Exercise3 {
     void generate_curve_figure_eight();
     void generate_curve_limacon();
     void generate_curve_3d();
+    void generate_world_axes();
 
     void laplacian_smoothing();
     void osculating_circle();
